6ca7f748972949eef2717d92c4fc2dcf9837d198
Gustavo Passos <guspassos@proton.me>
2025-08-18 10:42:22 -0300

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 # !/bin/bash
 
-# installing ohmyposh
-curl -s https://ohmyposh.dev/install.sh | bash -s
+
+if command -v oh-my-posh &> /dev/null; then
+    echo "Oh My Posh is already installed. Skipping installation."
+else
+    curl -s https://ohmyposh.dev/install.sh | bash -s
+fi
 
 dotfilesDir=$(pwd)
 
@@ -9,6 +13,22 @@ function linkDotfile {
   srcPath="${dotfilesDir}/${1}"
   dest="${HOME}/${1}"
   dateStr=$(date +%Y-%m-%d-%H%M)
+
+  if [ -d "${srcPath}" ]; then
+    echo "Processing directory: ${srcPath}"
+    mkdir -p "${dest}"
+    
+    find "${srcPath}" -mindepth 1 -maxdepth 1 -print0 | while IFS= read -r -d $'\0' item; do
+      itemName=$(basename "${item}")
+      newSrcPath="${srcPath}/${itemName}"
+      newDest="${dest}/${itemName}"
+      relativeSrcPath="${1}/${itemName}"
+      
+      # Recursively process each item
+      linkDotfile "${relativeSrcPath}"
+    done
+    return
+  fi
 
   mkdir -p "$(dirname "${dest}")"
 
@@ -26,10 +46,6 @@ function linkDotfile {
 
 
 linkDotfile .bashrc
-linkDotfile .config/nvim
-linkDotfile .config/konsolerc
-linkDotfile .config/kdeglobals
-linkDotfile .config/kglobalshortcutsrc
-linkDotfile .config/kplasmarc
-linkDotfile .config/kwinrc
-linkDotfile .local/share/kwin/scripts
+linkDotfile .config
+linkDotfile .local/share/kwin/scripts/krohnkite
+
