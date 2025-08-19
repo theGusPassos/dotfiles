6201bd811a95b10516ad69e532bb2263c897428e
Gustavo Passos <guspassos@proton.me>
2025-08-19 15:46:40 -0300

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,25 @@ fi
 
 dotfilesDir=$(pwd)
 
+function linkFile {
+  srcPath="${dotfilesDir}/${1}"
+  dest="${HOME}/${1}"
+  dateStr=$(date +%Y-%m-%d-%H%M)
+
+  mkdir -p "$(dirname "${dest}")"
+
+  if [ -h "${dest}" ]; then
+    echo "Removing existing symlink: ${dest}"
+    rm "${dest}"
+  elif [ -e "${dest}" ]; then
+    echo "Backing up existing: ${dest}"
+    mv "${dest}" "${dest}.${dateStr}"
+  fi
+
+  echo "Creating symlink: ${dest} -> ${srcPath}"
+  ln -s "${srcPath}" "${dest}"
+}
+
 linkFilesInFolder() {
   local srcPath="${dotfilesDir}/${1}"
   local destDir="${HOME}/${1}"
@@ -64,7 +83,7 @@ linkFolder() {
   ln -s "${srcPath}" "${dest}"
 }
 
-linkFilesInFolder /
+linkFile .bashrc
 linkFilesInFolder .config
 linkFolder .config/nvim
 linkFolder .local/share/kwin/scripts/krohnkite
