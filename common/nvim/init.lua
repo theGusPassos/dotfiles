713839b9869ef0eaa14f39b1297d65d209957be1
Gustavo Passos <guspassos@proton.me>
2025-11-13 00:01:46 -0300

    remove bg from nvim

--- common/nvim/init.lua
+++ common/nvim/init.lua
@@ -12,3 +12,15 @@ require("mason").setup({
 
 vim.cmd [[colorscheme kanagawa]]
 
+-- theme transparency
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+--vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+--vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "none" })
+vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+
