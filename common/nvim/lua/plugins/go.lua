82d26d13fac6b025fced7fa9760d48ad128a9ced
Gustavo Passos <guspassos@proton.me>
2025-11-20 23:16:13 -0300

    add lualine

--- common/nvim/lua/plugins/go.lua
+++ common/nvim/lua/plugins/go.lua
@@ -1,42 +1,42 @@
 return {
-    "ray-x/go.nvim",
-    dependencies = { -- optional packages
-        "ray-x/guihua.lua",
-        "neovim/nvim-lspconfig",
-        "nvim-treesitter/nvim-treesitter",
-    },
-    opts = {
-        lsp_keymaps = false,
-        dap_debug_keymap =  false,
-
-        lsp_inlay_hints = {
-            enable = true,
+    {
+        "ray-x/go.nvim",
+        dependencies = { -- optional packages
+            "ray-x/guihua.lua",
+            "neovim/nvim-lspconfig",
+            "nvim-treesitter/nvim-treesitter",
         },
+        opts = {
+            lsp_keymaps = false,
+            dap_debug_keymap = false,
 
-        -- prevent auto-downloads
-        go_install = false,
-        gopls_cmd = { "gopls" },
+            lsp_inlay_hints = {
+                enable = true,
+            },
+
+            -- prevent auto-downloads
+            go_install = false,
+            gopls_cmd = { "gopls" },
+        },
+        config = function(_, opts)
+            require("go").setup(opts)
+
+            local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                pattern = "*.go",
+                callback = function()
+                    require('go.format').goimports()
+                end,
+                group = format_sync_grp,
+            })
+            local gopls_cfg = require('go.lsp').config()
+            -- gopls_cfg.filetypes = { 'go', 'gomod'}, -- override settings
+
+            vim.lsp.config.gopls = gopls_cfg
+            vim.lsp.enable('gopls')
+        end,
+
+        ft = { "go", 'gomod' },
+        build = ':lua require("go.install").update_all_sync()'
     },
-    config = function(_, opts)
-        require("go").setup(opts)
-
-        local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = "*.go",
-            callback = function()
-                require('go.format').goimports()
-            end,
-            group = format_sync_grp,
-        })
-        local gopls_cfg = require('go.lsp').config()
-        -- gopls_cfg.filetypes = { 'go', 'gomod'}, -- override settings
-
-        print(gopls_cfg)
-
-        vim.lsp.config.gopls = gopls_cfg
-        vim.lsp.enable('gopls')
-    end,
-
-    ft = { "go", 'gomod' },
-    build = ':lua require("go.install").update_all_sync()'
 }
--- common/nvim/lua/plugins/others.lua
+++ common/nvim/lua/plugins/others.lua
@@ -1,20 +1,31 @@
 return {
     {
-      "j-hui/fidget.nvim",
-      opts = {
-        -- options
-      },
+        "j-hui/fidget.nvim",
+        opts = {
+            -- options
+        },
     },
     {
-      "iamcco/markdown-preview.nvim",
-      cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-      build = "cd app && yarn install",
-      init = function()
-        vim.g.mkdp_filetypes = { "markdown" }
-      end,
-      ft = { "markdown" },
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
     },
     {
         "sindrets/diffview.nvim",
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require('lualine').setup {
+                options = {
+                    theme = 'dracula',
+                },
+            }
+        end,
     }
 }
