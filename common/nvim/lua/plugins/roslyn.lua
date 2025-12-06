ea47a96b987adc8ac7ecdd335441729edf35c552
Gustavo Passos <guspassos@proton.me>
2025-12-06 14:17:43 -0300

    fix roslyn to not run on non-dotnet files

--- common/nvim/lua/plugins/roslyn.lua
+++ common/nvim/lua/plugins/roslyn.lua
@@ -1,40 +1,70 @@
-vim.lsp.config("roslyn", {
-    root_dir = function(fname)
-        local util = require("lspconfig").util
+-- plugins/csharp.lua or similar
+local lspconfig = require("lspconfig")
 
-        -- Scan current and parent directories manually:
-        return util.search_ancestors(fname, function(path)
-            for _, file in ipairs(vim.fn.readdir(path)) do
-                if file:match("%.sln$") or file:match("%.csproj$") then
-                    return path
+-- Helper function to detect if current directory is a C# project
+local function is_csharp_project()
+    local cwd = vim.loop.cwd()
+    for _, file in ipairs(vim.fn.readdir(cwd)) do
+        if file:match("%.sln$") or file:match("%.csproj$") then
+            return true
+        end
+    end
+    return false
+end
+
+-- ROSLYN LSP setup
+local function setup_roslyn()
+    lspconfig.roslyn.setup({
+        root_dir = function(fname)
+            local util = lspconfig.util
+            return util.search_ancestors(fname, function(path)
+                for _, file in ipairs(vim.fn.readdir(path)) do
+                    if file:match("%.sln$") or file:match("%.csproj$") then
+                        return path
+                    end
                 end
-            end
-        end)
-    end,
-    settings = {
-        ["csharp|inlay_hints"] = {
-            csharp_enable_inlay_hints_for_implicit_object_creation = true,
-            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+            end)
+        end,
+        settings = {
+            ["csharp|inlay_hints"] = {
+                csharp_enable_inlay_hints_for_implicit_object_creation = true,
+                csharp_enable_inlay_hints_for_implicit_variable_types = true,
+            },
+            ["csharp|code_lens"] = {
+                dotnet_enable_references_code_lens = true,
+            },
         },
-        ["csharp|code_lens"] = {
-            dotnet_enable_references_code_lens = true,
-        },
-    },
+    })
+end
+
+-- Auto-load Roslyn only for C# projects when opening a .cs file
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "cs",
+    callback = function()
+        if is_csharp_project() then
+            setup_roslyn()
+        end
+    end
 })
 
+-- PLUGIN SETUP TABLE (for lazy.nvim or packer.nvim)
 return {
     {
         "seblyng/roslyn.nvim",
         ft = { "cs", "csproj", "sln" },
         opts = {
-            filewatching = "roslyn",
+            filewatching = "polling", -- avoids dotnet dependency
         },
     },
     {
         "GustavEikaas/easy-dotnet.nvim",
-        dependencies = { "nvim-lua/plenary.nvim", 'nvim-telescope/telescope.nvim', },
+        dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
         config = function()
             require("easy-dotnet").setup()
-        end
+        end,
+        cond = function()
+            return is_csharp_project() -- only load in a C# project
+        end,
     },
 }
+
