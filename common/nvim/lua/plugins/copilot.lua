4cb4260e87fe5a8496df92dbf548c75bfd6ed641
Gustavo Passos <guspassos@proton.me>
2025-12-13 15:04:46 -0300

    disable copilot by default

--- common/nvim/lua/plugins/copilot.lua
+++ common/nvim/lua/plugins/copilot.lua
@@ -1,10 +1,8 @@
 return {
-  "github/copilot.vim",
-  init = function()
-    vim.g.copilot_filetypes = {
-      ["*"] = true,
-      txt = false,
-    }
-  end,
+    "github/copilot.vim",
+    init = function()
+        vim.g.copilot_filetypes = {
+            ["*"] = false,
+        }
+    end,
 }
-
