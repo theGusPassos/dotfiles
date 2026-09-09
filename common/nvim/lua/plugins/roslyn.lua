20e0f4a84e742034f2f3eee0beaf5648ce913274
Gustavo Passos <guspassos@proton.me>
2026-09-09 19:10:21 -0300

    fix roslyn

--- common/nvim/lua/plugins/roslyn.lua
+++ common/nvim/lua/plugins/roslyn.lua
@@ -7,37 +7,46 @@ return
     ---@module  'roslyn.config'
     ---@type    RoslynNvimConfig
     opts = {
-        filewatching = "roslyn",
+        -- "auto" | "roslyn" | "off"
+        --
+        -- - "auto": Does nothing for filewatching, leaving everything as default
+        -- - "roslyn": Turns off neovim filewatching which will make roslyn do the filewatching
+        -- - "off": Hack to turn off all filewatching. (Can be used if you notice performance issues)
+        filewatching = "auto",
 
-        broad_search = true,
+        -- Optional function that takes an array of targets as the only argument. Return the target you
+        -- want to use. If it returns `nil`, then it falls back to guessing the target like normal
+        -- Example:
+        --
+        -- choose_target = function(target)
+        --     return vim.iter(target):find(function(item)
+        --         if string.match(item, "Foo.sln") then
+        --             return item
+        --         end
+        --     end)
+        -- end
+        choose_target = nil,
 
-        choose_target = function(targets)
-            if #targets == 1 then
-                return targets[1]
-            end
+        -- Optional function that takes the selected target as the only argument.
+        -- Returns a boolean of whether it should be ignored to attach to or not
+        --
+        -- I am for example using this to disable a solution with a lot of .NET Framework code on mac
+        -- Example:
+        --
+        -- ignore_target = function(target)
+        --     return string.match(target, "Foo.sln") ~= nil
+        -- end
+        ignore_target = nil,
 
-            local cwd = vim.fn.getcwd() .. "/"
-            local labels = vim.tbl_map(function(t)
-                return t:gsub(vim.pesc(cwd), "")
-            end, targets)
+        -- Whether or not to look for solution files in the child of the (root).
+        -- Set this to true if you have some projects that are not a child of the
+        -- directory with the solution file
+        broad_search = false,
 
-            local chosen
-            vim.ui.select(labels, {
-                prompt = "Select Roslyn solution:",
-                format_item = function(item) return item end,
-            }, function(_, idx)
-                if idx then chosen = targets[idx] end
-            end)
-
-            return chosen
-        end,
-
-        ignore_target = function(target)
-            return false
-        end,
-
+        -- Whether or not to lock the solution target after the first attach.
+        -- This will always attach to the target in `vim.g.roslyn_nvim_selected_solution`.
+        -- NOTE: You can use `:Roslyn target` to change the target
         lock_target = false,
-        silent = false,
     },
 
     config = function(_, opts)
